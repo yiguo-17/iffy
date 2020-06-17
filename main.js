1f98474b3c9777df32f0cce8231c1e46fa13e851
play.js <git@gituser.com>
2020-06-17 12:47:46 -0400

    edit front end

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 // Your code here!
-
-
-
+const colorCombinator = require('./color-combinator.js')
+const colorDeconstructor = require('./color-deconstructor.js')
+const cl1 = getInput(0);
+const cl2 = getInput(1);
+if (cl1= 'undefined'){console.log('At least input one color.');}
+if( cl2= 'undefined'){
+  colorDeconstructor(cl1);
+}
+colorCombinator(cl1,cl2);
 // Our helper function here! Don't  touch!
 function getInput(i) {
   return process.argv[i + 2];
