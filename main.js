9b01d3bcb6358007abf10608f9261274f7c3a843
play.js <git@gituser.com>
2020-06-17 15:16:54 -0400

    fix typo

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ const colorCombinator = require('./color-combinator.js')
 const colorDeconstructor = require('./color-deconstructor.js')
 const cl1 = getInput(0);
 const cl2 = getInput(1);
-if (cl1=== 'undefined'){console.log('At least input one color.');}
-if( cl2=== 'undefined'){
+if (cl1=== undefined){console.log('At least input one color.');}
+if( cl2=== undefined){
   const message = colorDeconstructor(cl1);
   console.log(message);
 }
-if(cl2 !== 'undefined'){
+if(cl2 !== undefined){
   const message = colorCombinator(cl1,cl2);
   console.log(message);
 }
