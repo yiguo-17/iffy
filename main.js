e49a60eb73688a6bbf20ee7500be3ceca46a6b1c
play.js <git@gituser.com>
2020-06-17 13:07:33 -0400

    revise output

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ const cl1 = getInput(0);
 const cl2 = getInput(1);
 if (cl1=== 'undefined'){console.log('At least input one color.');}
 if( cl2=== 'undefined'){
-  colorDeconstructor(cl1);
+  const message = colorDeconstructor(cl1);
+  console.log(message);
 }
 if(cl2 !== 'undefined'){
-  colorCombinator(cl1,cl2);
+  const message = colorCombinator(cl1,cl2);
+  console.log(message);
 }
 // Our helper function here! Don't  touch!
 function getInput(i) {
