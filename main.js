98b8ebbce14299d2b5a579b34820d8f606c65262
play.js <git@gituser.com>
2020-06-17 12:58:55 -0400

    revise if syntax

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ const colorCombinator = require('./color-combinator.js')
 const colorDeconstructor = require('./color-deconstructor.js')
 const cl1 = getInput(0);
 const cl2 = getInput(1);
-if (cl1= 'undefined'){console.log('At least input one color.');}
-if( cl2= 'undefined'){
+if (cl1=== 'undefined'){console.log('At least input one color.');}
+if( cl2=== 'undefined'){
   colorDeconstructor(cl1);
 }
-colorCombinator(cl1,cl2);
+if(cl2 !== 'undefined'){
+  colorCombinator(cl1,cl2);
+}
 // Our helper function here! Don't  touch!
 function getInput(i) {
   return process.argv[i + 2];
